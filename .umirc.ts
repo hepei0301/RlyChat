fc59e0036fbf070ba91de03ad1d7c2609aa8c160
hepei0301 <916321685@qq.com>
2021-08-18 16:21:55 +0800

    init

--- .umirc.ts
+++ .umirc.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'umi';
+// import { defineConfig } from 'umi';
 
-export default defineConfig({
-  nodeModulesTransform: {
-    type: 'none',
-  },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-    { path: '/imChat', component: '@/pages/im/index' },
-    { path: '/meetChat', component: '@/pages/meet/index' },
-  ],
-  fastRefresh: {},
-});
+// export default defineConfig({
+//   nodeModulesTransform: {
+//     type: 'none',
+//   },
+//   routes: [
+//     { path: '/', component: '@/pages/index' },
+//     { path: '/imChat', component: '@/pages/im/index' },
+//     { path: '/meetChat', component: '@/pages/meet/index' },
+//   ],
+//   fastRefresh: {},
+// });
