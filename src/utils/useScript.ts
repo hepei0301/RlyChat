e51d214c141ae2a40d23615c2f274e0fd467868b
hepei0301 <916321685@qq.com>
2021-08-26 16:50:29 +0800

    init

--- src/utils/useScript.ts
+++ src/utils/useScript.ts
@@ -5,15 +5,15 @@ export const useScript = () => {
     if (!toggle) {
       toggle = true;
       const arrJs = [
-        'https://github.com/hepei0301/rly-chat/blob/main/public/clound/MD5.min.js',
-        'https://github.com/hepei0301/rly-chat/blob/main/public/clound/base64.min.js',
-        'https://github.com/hepei0301/rly-chat/blob/main/public/clound/jquery-3.1.0.min.js',
-        'https://github.com/hepei0301/rly-chat/blob/main/public/clound/ytx-web-im7.2.2.5.js',
-        'https://github.com/hepei0301/rly-chat/blob/main/public/clound/ytx-web-av3.js',
-        'https://github.com/hepei0301/rly-chat/blob/main/public/clound/adapter.js',
-        'https://github.com/hepei0301/rly-chat/blob/main/public/clound/config.js',
-        'https://github.com/hepei0301/rly-chat/blob/main/public/clound/RL_Meet.js',
-        'https://github.com/hepei0301/rly-chat/blob/main/public/clound/chatLogin.js',
+        'https://raw.githubusercontent.com/hepei0301/rly-chat/main/public/clound/MD5.min.js',
+        'https://raw.githubusercontent.com/hepei0301/rly-chat/main/public/clound/base64.min.js',
+        'https://raw.githubusercontent.com/hepei0301/rly-chat/main/public/clound/jquery-3.1.0.min.js',
+        'https://raw.githubusercontent.com/hepei0301/rly-chat/main/public/clound/ytx-web-im7.2.2.5.js',
+        'https://raw.githubusercontent.com/hepei0301/rly-chat/main/public/clound/ytx-web-av3.js',
+        'https://raw.githubusercontent.com/hepei0301/rly-chat/main/public/clound/adapter.js',
+        'https://raw.githubusercontent.com/hepei0301/rly-chat/main/public/clound/config.js',
+        'https://raw.githubusercontent.com/hepei0301/rly-chat/main/public/clound/RL_Meet.js',
+        'https://raw.githubusercontent.com/hepei0301/rly-chat/main/public/clound/chatLogin.js',
         'https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js',
       ];
       const arrCss = ['https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css'];
