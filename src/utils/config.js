b3ec9a12f912cac786394421c898cfaac85f759d
hepei0301 <916321685@qq.com>
2023-09-05 15:34:37 +0800

    fix:更改配置信息

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,9 +4,13 @@ var ip = {
   lvsServer: 'https://im.gcnao.cn:9002/accessFile', //
   mwsgServer: 'wss://im.gcnao.cn:9003/h5websocket', //
 };
-var app = {
-  _appid: '8a2af988536458c301537d7197320004', //'
-  _appToken: '0f26f16e4a8d4680a586c6eb2a9f4e03', // '
-};
+
+const storage = window.sessionStorage.getItem("IMCONFIG");
+var app = JSON.parse(storage ? storage : "{}");
+
+// var app = {
+//   _appid: '8a2af988536458c301537d7197320004', //'
+//   _appToken: '0f26f16e4a8d4680a586c6eb2a9f4e03', // '
+// };
 
 export { ip, app };
